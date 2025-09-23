21d34f42af775d0a5bd1b3167108414ac12e0192
sagarmasal12 <sagarmasal12@gmail.com>
2025-09-23 16:05:55 +0530

    server

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const mySqlPool = require("./config/db");
+const color = require("colors");
 
 const studentRoutes = require("./routes/studentRoute");
+
 
 //configuaration dotenv
 dotenv.config();
@@ -24,11 +26,11 @@ const PORT = process.env.PORT || 4000;
 mySqlPool
   .query("SELECT 1")
   .then(() => {
-    console.log("MYSQL DB Connected");
+    console.log("MYSQL DB Connected".bgGreen.red);
 
     //listen
     app.listen(PORT, () => {
-      console.log(`Server Running on Port:- http://localhost:4208 ${PORT}`);
+      console.log(`Server Running on Port:- http://localhost:4208 ${PORT}`.bgBlue);
     });
   })
   .catch((error) => {
