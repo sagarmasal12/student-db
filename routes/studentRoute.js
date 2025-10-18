82f0d2362890c7ae525162013d86a5b0102afcc7
sagarmasal12 <sagarmasal12@gmail.com>
2025-10-18 21:10:33 +0530

    api route

--- routes/studentRoute.js
+++ routes/studentRoute.js
@@ -1,11 +1,12 @@
 const express = require("express")
-const { getStudent, createStudent, deleteStudent } = require("../controllers/studentController")
+const { getStudent, createStudent, deleteStudent, getCourse } = require("../controllers/studentController")
 const router = express.Router()
 
 
 
 //getall student list
 router.get("/getall",getStudent)
+router.get("/getCourse",getCourse)
 
 router.post("/create",createStudent)
 
