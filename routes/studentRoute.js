03f9c1fb1ed0496cb39c082a138ad1e60341e3ed
sagarmasal12 <sagarmasal12@gmail.com>
2025-11-04 16:42:45 +0530

    uncomment

--- routes/studentRoute.js
+++ routes/studentRoute.js
@@ -1,16 +1,16 @@
-// const express = require("express")
-// const { getStudent, createStudent, deleteStudent, getCourse } = require("../controllers/studentController")
-// const router = express.Router()
+const express = require("express")
+const { getStudent, createStudent, deleteStudent, getCourse } = require("../controllers/studentController")
+const router = express.Router()
 
 
 
-// //getall student list
-// router.get("/getall",getStudent)
-// router.get("/getCourse",getCourse)
+//getall student list
+router.get("/getall",getStudent)
+router.get("/getCourse",getCourse)
 
-// router.post("/create",createStudent)
+router.post("/create",createStudent)
 
-// router.delete("/delete/:id",deleteStudent)
+router.delete("/delete/:id",deleteStudent)
 
 
-// module.exports = router;
+module.exports = router;
